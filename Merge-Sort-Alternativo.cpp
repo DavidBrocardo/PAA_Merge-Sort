b6c67c3d5bb8161854a16cbcfc11797b74a7e0d8
davidbrocardo@gmail.com <70301267+DavidBrocardo@users.noreply.github.com>
2024-11-14 10:23:33 -0300

    Rodando os arquivos

--- Merge-Sort-Alternativo.cpp
+++ Merge-Sort-Alternativo.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
+#include <time.h>
 using namespace std;
-
 
 void merge_in_place(vector<int>& arr, int inicio, int meio, int fim) {
     int i = inicio;  
     int j = meio + 1; 
 
     while (i <= meio && j <= fim) {
-
         if (arr[i] <= arr[j]) {
             i++;
         } else {
-
             int aux = arr[j];
             for (int k = j; k > i; k--) {
                 arr[k] = arr[k - 1];
             }
             arr[i] = aux;
-            
             i++;
             meio++;
             j++;
@@ -26,29 +25,53 @@ void merge_in_place(vector<int>& arr, int inicio, int meio, int fim) {
     }
 }
 
-
 void merge_sort_in_place(vector<int>& arr, int inicio, int fim) {
     if (inicio < fim) {
         int meio = inicio + (fim - inicio) / 2;
-
-        
         merge_sort_in_place(arr, inicio, meio);
         merge_sort_in_place(arr, meio + 1, fim);
-
-        
         merge_in_place(arr, inicio, meio, fim);
     }
 }
 
-int main() {
-    vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cerr << "Uso: " << argv[0] << " <nome_do_arquivo>" << endl;
+        return 1;
+    }
 
-    merge_sort_in_place(arr, 0, arr.size() - 1);
+    ifstream arquivo(argv[1]);
+    if (!arquivo.is_open()) {
+        cerr << "Erro ao abrir o arquivo." << endl;
+        return 1;
+    }
 
-    
-    cout << "Vetor ordenado: ";
-    for (int num : arr)
+    vector<int> vetor;
+    string linha;
+    while (getline(arquivo, linha)) {
+        try {
+            int numero = stoi(linha);
+            vetor.push_back(numero);
+        } catch (invalid_argument& e) {
+            cerr << "Erro ao converter linha para número: " << linha << endl;
+        }
+    }
+    arquivo.close();
+
+    /*cout << "Vetor antes da ordenação: ";
+    for (int num : vetor) {
         cout << num << " ";
+    }
+    cout << endl;*/
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+    merge_sort_in_place(vetor, 0, vetor.size() - 1);
+    double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Tempo gasto: %g ms.", Tempo);
+    cout << "Vetor ordenado: ";
+    for (int num : vetor) {
+        cout << num << " ";
+    }
     cout << endl;
 
     return 0;
